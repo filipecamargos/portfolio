83724dadc97f5e62e8676d9939128a0508aee623
filipecamargos <filipe.atrabalho@gmail.com>
2022-03-14 21:05:29 -0600

    sidebar layout and style added

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -2,6 +2,11 @@ import cssClasses from "./Sidebar.module.css";
 
 const Sidebar = () => {
   const name = "Filipe Ferreira";
+  const linkedin = "https://www.linkedin.com/in/filipe-ferreira-eng/";
+  const github = "https://github.com/filipecamargos";
+  const facebook = "https://www.facebook.com/filipe.camargos.902/";
+  const email = "mailto: filipe.atrabalho@gmail.com";
+
   const interests = ["#Software_Engineering", "#Learning", "#Coding"].map(
     (el, i) => {
       return (
@@ -11,6 +16,7 @@ const Sidebar = () => {
       );
     }
   );
+
   return (
     <div className={cssClasses.sidenav}>
       <img src="/profile.jpg" alt="profile" />
@@ -22,10 +28,23 @@ const Sidebar = () => {
         <a href="#contact">Contact</a>
       </div>
       <div className={cssClasses.icons}>
-        <a href="#about"><i class="fa-brands fa-linkedin-in"></i></a>
-        <a href="#services"><i class="fa-brands fa-github"></i></a>
-        <a href="#services"><i class="fa fa-envelope"></i></a>
-        <a href="#services"><i class="fa-brands fa-facebook"></i></a>
+        <div>
+          <a href={linkedin}>
+            <i className="fa-brands fa-linkedin-in"></i>
+          </a>
+          <a href={github}>
+            <i className="fa-brands fa-github"></i>
+          </a>
+        </div>
+      <div>
+      <a href={email} target="_blank">
+          <i className="fa fa-envelope"></i>
+        </a>
+        <a href={facebook}>
+          <i className="fa-brands fa-facebook"></i>
+        </a>
+      </div>
+
       </div>
       <div className={cssClasses.interests}>{interests}</div>
     </div>
