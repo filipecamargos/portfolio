1dc102970f9184826b1eb2a73ed5e1ec9f946cd9
filipecamargos <filipe.atrabalho@gmail.com>
2022-03-26 12:31:12 -0600

    Layout for Projects Ready

--- src/components/MainContent/Projects/Projects.js
+++ src/components/MainContent/Projects/Projects.js
@@ -5,39 +5,56 @@ const Projects = () => {
 
   const projectList = [
     {
-      data: 'Apr 2022',
+      img: "\sloop.png",
       title: 'Sloop: Team Management Feedback',
       description: 'Sloop App is an application that allow management to manage their teams and provide feeback.',
       stack: 'HTML, CSS, JS, NodeJS, MongoDB, Heroku',
       gitHub: 'https://intermountainhealthcare.org/'
     },
     {
-      data: 'Apr 2022',
+      img: "\sloop.png",
       title: 'Sloop: Team Management Feedback',
       description: 'Sloop App is an application that allow management to manage their teams and provide feeback.',
       stack: 'HTML, CSS, JS, NodeJS, MongoDB, Heroku',
       gitHub: 'https://intermountainhealthcare.org/'
     },
     {
-      data: 'Apr 2022',
+      img: "\sloop.png",
+      title: 'Sloop: Team Management Feedback',
+      description: 'Sloop App is an application that allow management to manage their teams and provide feeback.',
+      stack: 'HTML, CSS, JS, NodeJS, MongoDB, Heroku',
+      gitHub: 'https://intermountainhealthcare.org/'
+    },
+    {
+      img: "\sloop.png",
+      title: 'Sloop: Team Management Feedback',
+      description: 'Sloop App is an application that allow management to manage their teams and provide feeback.',
+      stack: 'HTML, CSS, JS, NodeJS, MongoDB, Heroku',
+      gitHub: 'https://intermountainhealthcare.org/'
+    },
+    {
+      img: "\sloop.png",
       title: 'Sloop: Team Management Feedback',
       description: 'Sloop App is an application that allow management to manage their teams and provide feeback.',
       stack: 'HTML, CSS, JS, NodeJS, MongoDB, Heroku',
       gitHub: 'https://intermountainhealthcare.org/'
     },
     
-  ].map((exp, key) => {
-    return (<div className={cssClassesExp.experienceInfo} key={key}>
-      <p><b>{exp.position}</b></p>
-      <a href={exp.companySite}><i>{exp.company}</i></a>
-      <p>{exp.data}</p>
+  ].map((proj, key) => {
+    return (<div className={cssClassesExp.experienceInfo + " " + cssClasses.project} key={key}>
+      <img src={proj.img} alt="Sloop"/>
+      <p>{proj.title}</p>
+      <p>{proj.description}</p>
+      <p>{proj.stack}</p>
+      <p>{proj.data}</p>
+      <p><a href={proj.gitHub}>{proj.gitHub}</a></p>
       <hr />
     </div>)
   });
 
   return (
     <div className={cssClassesExp.experienceContent}>
-      <h1 className="animate__slideInLeft"><i className="fa fa-briefcase" aria-hidden="true"></i> Experience</h1>
+      <h1 className="animate__slideInLeft"><i class="fa fa-file-code-o" aria-hidden="true"></i> My Projects</h1>
       <section className={cssClassesExp.experienceSection + " animate__fadeIn"} >
         {projectList}
       </section>
