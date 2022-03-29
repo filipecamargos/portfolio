5475ae536d2e366a5bce471c754747700408650c
filipecamargos <filipe.atrabalho@gmail.com>
2022-03-28 22:23:55 -0600

    Fix anchor content error

--- src/components/Mobilebar/Mobilebar.js
+++ src/components/Mobilebar/Mobilebar.js
@@ -1,21 +1,36 @@
-import cssClasses from "./Mobilebar.module.css"
+import cssClasses from "./Mobilebar.module.css";
 
 const Mobilebar = (props) => {
-    return (
-        <div className={cssClasses.navbar}>
-            <a href="#home" onClick={props.onHomeClick}><i class="fa fa-home" aria-hidden="true"></i></a>
-            <a href="#about" className={props.contentDisplay === 'About' ? cssClasses.active : ''} onClick={props.onAboutClick}>
-            <i class="fa fa-user-circle-o" aria-hidden="true"></i>
-            </a>
-            <a href="#projects" className={props.contentDisplay === 'Projects' ? cssClasses.active : ''} onClick={props.onProjectClick}>
-            <i class="fa fa-code" aria-hidden="true"></i>
-            </a>
-            <a href="#experience" className={props.contentDisplay === 'Experience' ? cssClasses.active : ''} onClick={props.onExperienceClick}>
-            <i class="fa fa-briefcase" aria-hidden="true"></i>
-            </a>
+  return (
+    <div className={cssClasses.navbar}>
+      <a href="#home" onClick={props.onHomeClick}>
+        <i class="fa fa-home" aria-hidden="true"></i>
+      </a>
+      <a
+        href="#about"
+        className={props.contentDisplay === "About" ? cssClasses.active : ""}
+        onClick={props.onAboutClick}
+      >
+        <i class="fa fa-user-circle-o" aria-hidden="true"></i>
+      </a>
+      <a
+        href="#projects"
+        className={props.contentDisplay === "Projects" ? cssClasses.active : ""}
+        onClick={props.onProjectClick}
+      >
+        <i class="fa fa-code" aria-hidden="true"></i>
+      </a>
+      <a
+        href="#experience"
+        className={
+          props.contentDisplay === "Experience" ? cssClasses.active : ""
+        }
+        onClick={props.onExperienceClick}
+      >
+        <i class="fa fa-briefcase" aria-hidden="true"></i>
+      </a>
+    </div>
+  );
+};
 
-        </div>
-    )
-}
-
-export default Mobilebar
+export default Mobilebar;
